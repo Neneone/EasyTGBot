c4d7192aef43dbf115f68c7dd9a7f67ff374b331
Enea Dolcini <eneadolcini@gmail.com>
2018-09-12 17:01:30 +0200

    Migliorati i controlli
    In questa commit sono stati migliorati i controlli prima dell'inizializzazione della TGBot.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include 'TGBot.php';
+if(!isset($_GET['fpam']) || !isset($_GET['token'])) die('Missing parameters.');
 $TGBot = new TGBot(file_get_contents("php://input"), 'FPAM', $_GET['fpam'], $_GET['token']);
 $TGBot->SecTest();
 include 'conf.php';
